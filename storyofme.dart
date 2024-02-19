a5511fcdbd13b670355363f13bc0793527da2f1f
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2024-02-19 15:57:12 +0530

    Add files via upload

---
+++ storyofme.dart
@@ -0,0 +1,43 @@
+
+
+//import 'package:storyofme/storyofme.dart' as storyofme;
+import 'dart:io';
+
+main() 
+{
+  print("Enter your name");
+ String name=stdin.readLineSync()?? '';
+  
+  print("Enter your phone number");
+ String num=stdin.readLineSync()?? '';
+  
+  print("Enter your age");
+  int age=int.parse(stdin.readLineSync()?? '');
+   
+
+  print("Enter your height");
+ double height=double.parse(stdin.readLineSync()?? '');
+   
+ 
+  
+  print("Enter your weight");
+  double weight=double.parse(stdin.readLineSync()?? '');
+   
+  
+ print("Enter your Address");
+ String  address=stdin.readLineSync()?? '';
+
+  print("Enter your Hobbies\n");
+  List<String>hobbies=(stdin.readLineSync()?? '').split(',');
+
+print("BIODATA ");
+
+print('\nName:$name');
+print('Phone Number:$num');
+print('Age:$age');
+print('Height:$height');
+print('Weight:$weight');
+print('Address:$address');
+print("Hobbies:$hobbies");
+
+}
